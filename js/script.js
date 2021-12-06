b1d67dfef63edee24fcb28bd80601e4604f010b6
Melis <tyn.melis.ru@gmail.com>
2021-12-06 21:19:25 +0300

    Добавил js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,29 @@ hamburger.addEventListener('click', function() {
   nav.classList.toggle('active')
 
 })
+
+let tagLinks = document.querySelectorAll('.tags__link');
+let infos = document.querySelectorAll('.news__info');
+let tagWrap = document.querySelector('.side__tags');
+
+
+tagLinks.forEach(item => {
+  item.addEventListener('click', function(e) {
+    e.preventDefault();
+    for (const item of tagWrap.children) {
+      item.classList.remove('active')
+    }
+
+    if (!this.classList.contains('active')) {
+      let [,hash] = this.getAttribute('href').split('#');
+      infos.forEach(info => {
+        
+        info.classList.remove('active')
+        if ( hash === info.dataset.desc ) {
+          this.classList.add('active');
+          info.classList.add('active')
+        }
+      })
+    }
+  })
+})
